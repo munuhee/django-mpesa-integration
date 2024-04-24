7d43967c9b5976633444b73e538ea3f049ae2a2a
munuhee <murichustephen@gmail.com>
2024-04-24 09:47:29 +0300

    fix error

--- mpesa_transactions/models.py
+++ mpesa_transactions/models.py
@@ -5,14 +5,6 @@ class MPesaTransaction(models.Model):
     """
     Model representing an M-Pesa transaction.
     """
-
-    #merchant_request_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="Merchant Request ID")
-    #checkout_request_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="Checkout Request ID")
-    #response_code = models.CharField(max_length=10, verbose_name="Response Code", db_index=True)
-    #response_description = models.CharField(max_length=255, verbose_name="Response Description")
-    #customer_message = models.CharField(max_length=255, verbose_name="Customer Message")
-    #timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Timestamp")
-
     phone_number = models.CharField(max_length=15, verbose_name="Phone Number")
     amount = models.IntegerField(verbose_name="Amount")  # Using DecimalField for better precision
     account_reference = models.CharField(max_length=50, verbose_name="Account Reference")
@@ -26,4 +18,4 @@ class MPesaTransaction(models.Model):
         """
         String representation of the M-Pesa transaction.
         """
-        return f"{self.merchant_request_id} - {self.response_code}"
+        return f"{self.phone_number} - {self.amount}"
